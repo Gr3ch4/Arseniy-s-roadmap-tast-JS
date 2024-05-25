7ee002df974c043cd12422ce09c0e9464b2adf98
Gr3ch4 <grisha.salmin.2002@mail.ru>
2024-05-25 17:08:57 +0300

    add validation, add error message, add opening of contact details

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
 const contactList = []
 
+const namesFirstLetter = []
 
-
-const alphabetMap = alphabet.map((letter) => `<div class="contact-item">${letter}</div>`).join('')
+const alphabetMap = alphabet.map((letter) => `<div id="${letter}" class="contact-item"><p class="contact-item-letter">${letter}</p></div>`).join('')
 
 document.querySelector('.contact-list').innerHTML = alphabetMap
 
@@ -16,17 +16,73 @@ function Contact(name, vacancy, phone) {
     
 document.forms.form.onsubmit = function(e) {
     e.preventDefault()
-    let name = this.name.value
-    let vacancy = this.vacancy.value
-    let phone = this.phone.value
+    let name = this.name.value.trim()
+    let vacancy = this.vacancy.value.trim()
+    let phone = this.phone.value.trim()
+
+    const errorMessage = document.querySelector('.error')
+    errorMessage.innerHTML = '' // Очистка предыдущих сообщений об ошибках
+
+    if(name === '' || vacancy === '' || phone === '') {
+        errorMessage.innerHTML = 'Вы не ввели имя, вакансию или номер телефона'
+        return // Прекращаем выполнение функции, если есть ошибки
+    } 
+
+    const nameExist = contactList.some(contact => contact.name === name)
+    const phoneExist = contactList.some(contact => contact.phone === phone)
+
+    if(nameExist) {
+        errorMessage.innerHTML = 'Имя уже занято'
+        return
+    }
+    if(phoneExist) {
+        errorMessage.innerHTML = 'Контакт с этим номером уже записан'
+        return
+    }
 
     let contact = new Contact(name, vacancy, phone)
-
-    console.log(contact)
-
     contactList.push(contact)
 
-    setTimeout(()=>{
-        console.log(contactList)
-    }, 2000)
+    namesFirstLetter.push(contact.name[0])
+    
+    const counts = {}
+
+    namesFirstLetter.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; })
+
+    const lastLetter = namesFirstLetter[namesFirstLetter.length - 1];
+    const counterText = `${counts[lastLetter]}`
+    
+    // Проверяем, есть ли уже элемент с количеством, и если нет, то создаем его
+    const letterDiv = document.querySelector(`#${lastLetter}`);
+    let counterElement = letterDiv.querySelector('.contact-item-number');
+    
+    if (counterElement) {
+        counterElement.textContent = counterText;
+    } else {
+        counterElement = document.createElement('p');
+        counterElement.className = 'contact-item-number';
+        counterElement.textContent = counterText;
+        letterDiv.appendChild(counterElement);
+    }
+
+    console.log(namesFirstLetter)
 }
+
+const allLetters = document.querySelector('.contact-list')
+
+const handleClick = (e) => {
+    // Ищем ближайший родительский элемент, который имеет атрибут id
+    const letterDiv = e.target.closest('.contact-item');
+    
+    const insideLetter = document.querySelector(`#${letterDiv.id}`)
+
+    if (letterDiv && letterDiv.id) {
+        // Проверяем, существует ли уже элемент с классом 'contact-details'
+        if (!letterDiv.querySelector('.contact-details')) {
+            // Вставляем новый элемент в конец letterDiv, если его еще нет
+            letterDiv.insertAdjacentHTML('beforeend', `<div class="contact-details">Подробный контакт</div>`);
+        }
+    }
+}
+
+allLetters.addEventListener('click', handleClick)
